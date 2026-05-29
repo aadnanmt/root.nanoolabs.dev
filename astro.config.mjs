fc9d65a4af879265baf8737eae65145fa4c4580f
Adnan Slamet Wibowo <contact@aadnanmt.web.id>
2026-05-29 15:46:16 +0700

    fix:  git hash injection and dynamic year active

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { execSync } from "child_process";
 
 const isProd = process.env.CF_PAGES === "1";
+
+let gitHash = "unknown";
+try {
+  gitHash = execSync("git rev-parse --short HEAD").toString().trim();
+} catch (e) {
+  console.warn("Could not get git hash");
+}
 
 let adapter;
 if (isProd) {
@@ -19,4 +27,11 @@ export default defineConfig({
   },
   output: isProd ? "server" : "static",
   adapter: adapter,
+  vite: {
+    define: {
+      "import.meta.env.GIT_HASH": JSON.stringify(
+        process.env.CF_PAGES_COMMIT_SHA?.substring(0, 7) || gitHash,
+      ),
+    },
+  },
 });
