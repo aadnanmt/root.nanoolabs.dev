59a9a92d17a45e512cceabb3e078cf0c465258d2
Adnan Slamet Wibowo <adnan@nanoolabs.dev>
2026-06-08 09:35:44 +0700

    refactor(focus): separate data from UI and integrate new progress component

---
+++ src/data/focus.ts
@@ -0,0 +1,26 @@
+export const MYFOCUS = [
+  {
+    title: "System & DevOps",
+    percentage: 39,
+    description:
+      "Deep dive filesystem, mechanism symlink, config bootloader, permission, exploring rootless Podman, other Git feature, and Bash fundamentals.",
+  },
+  {
+    title: "Backend Engineering",
+    percentage: 16,
+    description:
+      "Implementing Pnpm Workspace and Hono. Exploring Middleware and Router on Hono",
+  },
+  {
+    title: "Frontend & UI",
+    percentage: 9,
+    description:
+      "Optimize SEO and performance. Learn and manage clean Tailwind CSS and mastering Astro Middleware",
+  },
+  {
+    title: "Testing & Maintainace",
+    percentage: 23,
+    description:
+      "Analyzing Hyprland and using Nix Shell on my Distro. Reconfigure Termux Properties for efficient workflow",
+  },
+]
